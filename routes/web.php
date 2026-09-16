8e456c68e692a72340a9aa2c31d18d5ad2940e27
DinaNampoina <dinanampoina18@gmail.com>
2026-09-16 13:28:24 +0300

    feat: ajout annonces resource routes et auth protection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnonceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,12 +10,20 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('annonces', AnnonceController::class)
+    ->except(['create', 'store', 'edit', 'update', 'destroy']);
+
+Route::middleware('auth')->group(function () {
+    Route::resource('annonces', AnnonceController::class)
+        ->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
